7c0e971b1e9174224dffd1385281bb2680f45c32
Amanda Pattridge <alpattridge@gmail.com>
2016-04-04 15:07:26 -0700

    Fix port assignment

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
 
 var port  = process.env.PORT|| 3000;
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
