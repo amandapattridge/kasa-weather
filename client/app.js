4c71531a70015c2ba92f3f5177366c5ee256cf49
Amanda Pattridge <alpattridge@gmail.com>
2016-04-05 14:49:37 -0700

    Attempt to route ajax through server

--- client/app.js
+++ client/app.js
@@ -1,12 +1,24 @@
-// var getWeather = function(url, cb){
-// 	var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q="+loc+"&units=imperial";
-// 	$get(url, function(results){
-		
-// 	});
-// };
-
-// var loc = $('#zip').val();
-// $('button').on('click', function(){
-// 	getWeather(loc);
-// 	console.log(loc);
-// });
+var getWeather = function(loc, cb){
+      $.getJSON('//api.openweathermap.org/data/2.5/weather?q='+loc+'&units=imperial&APPID=5108ea3af1fe4418cd038843b7e3cf53').then(function(results){
+        // console.log(results);
+        console.log(results);
+        if(cb){
+          cb(results);
+        }
+      });
+    };
+    
+  var getWeatherOnClick = function(){
+   	return $('button').on('click', function(e){
+      var location = $('#zip').val();
+        getWeather(location, function(data){
+          console.log(data);
+          $('#results').empty()
+          $('#results').append('<h4>'+data.name+'</h4>');
+          $('#results').append('<div id="condition"><h5>'+data.weather[0].description+'</h5></div>')
+          $('#results').append('<div id="temp"> Temp: '+data.main.temp+' F</div>')
+          $('#results').append('<div id="wind"> Wind: '+data.wind.speed+' mph</div>')
+          $('#results').append('<div id="humidity"> Humidity: '+data.main.humidity+'%</div>')
+        });
+      });
+    };
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = require('./client/app.js');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use("/client", express.static(__dirname + '/client'));
+app.use("/bower_components", express.static(__dirname + '/bower_components'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
   res.render('main');
 });
 
 app.post('/', function(req, res){
-  console.log('post request made')
+  // res.send("Hello")
+  // var zip = req.body.zip;
+  // var data;
+  // app.get('//api.openweathermap.org/data/2.5/weather?q='+zip+'&units=imperial&APPID=5108ea3af1fe4418cd038843b7e3cf53', function(req, res){
+  //     res.end(function(){
+  //       data = res.body;
+  //     })
+  // })
+  // res.end(data + "Hello!");
+
 });
 
 var port  = process.env.PORT|| 3000;
