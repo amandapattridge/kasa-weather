e742f9bca960e65b63bb5f2a6157cc96eb608807
Amanda Pattridge <alpattridge@gmail.com>
2016-04-05 17:06:32 -0700

    Gets and displays weather data through server

--- client/app.js
+++ client/app.js
@@ -1,24 +1,30 @@
-var getWeather = function(loc, cb){
-      $.getJSON('//api.openweathermap.org/data/2.5/weather?q='+loc+'&units=imperial&APPID=5108ea3af1fe4418cd038843b7e3cf53').then(function(results){
-        // console.log(results);
-        console.log(results);
-        if(cb){
-          cb(results);
-        }
+var $ = require("jquery");
+
+var weatherFuncs = {
+  getWeather: function(loc, cb){
+        $.getJSON('//api.openweathermap.org/data/2.5/weather?q='+loc+'&units=imperial&APPID=5108ea3af1fe4418cd038843b7e3cf53').then(function(results){
+      // console.log(results);
+      console.log(results);
+      if(cb){
+        cb(results);
+      }
+    });
+  },
+      
+  getWeatherOnClick : function(){
+  	return $('button').on('click', function(e){
+    var location = $('#zip').val();
+      getWeather(location, function(data){
+        console.log(data);
+        $('#results').empty();
+        $('#results').append('<h4>'+data.name+'</h4>');
+        $('#results').append('<div id="condition"><h5>'+data.weather[0].description+'</h5></div>');
+        $('#results').append('<div id="temp"> Temp: '+data.main.temp+' F</div>');
+        $('#results').append('<div id="wind"> Wind: '+data.wind.speed+' mph</div>');
+        $('#results').append('<div id="humidity"> Humidity: '+data.main.humidity+'%</div>');
       });
-    };
-    
-  var getWeatherOnClick = function(){
-   	return $('button').on('click', function(e){
-      var location = $('#zip').val();
-        getWeather(location, function(data){
-          console.log(data);
-          $('#results').empty()
-          $('#results').append('<h4>'+data.name+'</h4>');
-          $('#results').append('<div id="condition"><h5>'+data.weather[0].description+'</h5></div>')
-          $('#results').append('<div id="temp"> Temp: '+data.main.temp+' F</div>')
-          $('#results').append('<div id="wind"> Wind: '+data.wind.speed+' mph</div>')
-          $('#results').append('<div id="humidity"> Humidity: '+data.main.humidity+'%</div>')
-        });
-      });
-    };
+    });
+  }
+};
+
+module.exports = weatherFuncs
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser')
-var app = require('./client/app.js');
+var bodyParser = require('body-parser');
+var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
+// var jsdom = require('jsdom')
+var unirest = require('unirest');
+var weatherFuncs = require('./client/app.js');
 var app = express();
 
 app.set('views', __dirname + '/views');
@@ -15,17 +18,34 @@ app.get('/', function (req, res) {
   res.render('main');
 });
 
-app.post('/', function(req, res){
-  // res.send("Hello" )
-  // var zip = req.body.zip;
-  // var data;
-  // app.get('//api.openweathermap.org/data/2.5/weather?q='+zip+'&units=imperial&APPID=5108ea3af1fe4418cd038843b7e3cf53', function(req, res){
-  //     res.end(function(){
-  //       data = res.body;
-  //     })
-  // })
-  // res.end(data + "Hello!");
+var getWeatherData = function(zip, cb){
+  unirest.get('http://api.openweathermap.org/data/2.5/weather?q='+zip+'&units=imperial&APPID=5108ea3af1fe4418cd038843b7e3cf53').end(function(response){
+      cb(response.body)
+      console.log(response.body);
+  })
+}
 
+var addWeatherDataToPage = function(data){
+  $('#results').empty();
+  $('#results').append('<h4>'+data.name+'</h4>');
+  $('#results').append('<div id="condition"><h5>'+data.weather[0].description+'</h5></div>');
+  $('#results').append('<div id="temp"> Temp: '+data.main.temp+' F</div>');
+  $('#results').append('<div id="wind"> Wind: '+data.wind.speed+' mph</div>');
+  $('#results').append('<div id="humidity"> Humidity: '+data.main.humidity+'%</div>');
+};
+
+app.post('/', function(req, res){
+  var zip = req.body.zip;
+  console.log('zip', zip);
+
+  // res.end(weatherFuncs.getWeather());
+  unirest.get('http://api.openweathermap.org/data/2.5/weather?q='+zip+'&units=imperial&APPID=5108ea3af1fe4418cd038843b7e3cf53').end(function(response){
+      var place = response.body.name;
+      var temp = response.body.main.temp;
+      var humidity = response.body.main.humidity;
+      var wind = response.body.wind.speed;
+      res.render('results.ejs', {'place': place, 'wind': wind, 'humidity': humidity, 'temp': temp})
+  });
 });
 
 var port  = process.env.PORT|| 3000;
